73df051056fdc30b5186525f5eab645aa235ddc7
Martin Spier <spiermar@gmail.com>
2026-02-14 20:57:54 +0000

    feat: add OFX parser module

---
+++ lib/ofx/types.ts
@@ -0,0 +1,25 @@
+export interface OfxTransaction {
+  fitid: string;
+  refnum: string;
+  memo: string;
+  date: string; // YYYY-MM-DD
+  amount: number; // positive = credit, negative = debit
+  type: 'CREDIT' | 'DEBIT';
+}
+
+export interface OfxAccount {
+  bankId: string;
+  accountId: string;
+  type: string;
+}
+
+export interface OfxDateRange {
+  start: string;
+  end: string;
+}
+
+export interface ParsedOfxImport {
+  account: OfxAccount;
+  dateRange: OfxDateRange;
+  transactions: OfxTransaction[];
+}
